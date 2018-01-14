6c0290ea867b8d3aedb1be49c426ed9160a24ab8
Juan Luis Gamella Martín <juangamella@gmail.com>
2018-01-14 02:34:25 +0100

    Test module for in_app diagnostics

---
+++ test/bank_tests.erl
@@ -0,0 +1,55 @@
+%% @author Juan Luis Gamella Martin
+%%
+%% See LICENSE.txt file for detailed information.
+%%
+%% @doc This module contains top-level tests for the bank application
+
+-module(bank_tests).
+
+%% Behaviour
+
+%% Includes
+
+%% Exports
+-export([
+         run/0
+        ]).
+
+%% Macro definitions
+
+%% Type Definitions
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Public functions
+
+run() ->
+  Tests = open_account_tests(),
+  lists:foreach(fun(F) ->
+                    F()
+                end, Tests).
+  
+open_account_tests() ->
+  {ok, Bank} = application:get_env(bank, bank_code),
+    {ok, #{id := C1}} = bn_logic:new_customer(<<"customer_1">>),
+    {ok, #{id := C2}} = bn_logic:new_customer(<<"customer_2">>),
+  [ 
+    fun() ->
+        {ok, #{id := IBAN}} = bn_logic:open_account(C1, <<"EUR">>),
+        <<_:4/binary, BankCode:4/binary, _/binary>> = IBAN,
+        Bank = BankCode
+    end,
+    fun() ->
+        {ok, #{id := IBAN}} = bn_logic:open_account(C1, <<"EUR">>),
+        <<_:4/binary, BankCode:4/binary, _/binary>> = IBAN,
+        Bank = BankCode
+    end,
+    fun() ->
+        {ok, #{id := IBAN}} = bn_logic:open_account(C2, <<"EUR">>),
+        <<_:4/binary, BankCode:4/binary, _/binary>> = IBAN,
+        Bank = BankCode
+    end
+  ].
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Internal functions
+
